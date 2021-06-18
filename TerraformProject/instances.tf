5c6276135d8f220b9522740e5855d95ec3181928
Benoniy <benranson5@gmail.com>
2021-06-18 10:59:49 +0100

    terraform is now fully separated from k8

--- TerraformProject/instances.tf
+++ TerraformProject/instances.tf
@@ -1,3 +1,17 @@
+resource "aws_instance" "privateInstance" {
+    ami = "ami-0308a1cade9976b8e"
+    key_name = "ho-pa-project"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.private_1a.id
+    associate_public_ip_address = true
+    private_ip = "69.69.69.194"
+    vpc_security_group_ids = [aws_security_group.main.id]
+
+    tags = {
+        Name = "ho_k8_master"
+    }
+}
+
 resource "aws_instance" "firstInstance" {
     ami = "ami-015e12b7e80c0bf5d"
     key_name = "ho-pa-project"
@@ -29,22 +43,12 @@ resource "aws_instance" "secondInstance" {
     depends_on = [aws_instance.privateInstance]
 }
 
-resource "aws_instance" "privateInstance" {
-    ami = "ami-0308a1cade9976b8e"
-    key_name = "ho-pa-project"
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.private_1a.id
-    associate_public_ip_address = true
-    private_ip = "69.69.69.194"
-    vpc_security_group_ids = [aws_security_group.main.id]
-
-    tags = {
-        Name = "ho_k8_master"
-    }
+resource "local_file" "master_ids" {
+    content  = aws_instance.privateInstance.id
+    filename = "/home/ubuntu/terraFiles/master_ids"
 }
 
-
-resource "local_file" "instance_ids" {
-    content  = aws_instance.privateInstance.id
-    filename = "instance_ids.txt"
+resource "local_file" "worker_ids" {
+    content  = "${aws_instance.firstInstance.id}\n${aws_instance.secondInstance.id}"
+    filename = "/home/ubuntu/terraFiles/worker_ids"
 }
